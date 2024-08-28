9e5ce835385aad098208fa97b0d2cbc5d7a1dbda
Mashiro <thomas.goliu@gmail.com>
2024-08-28 01:02:49 -0400

    Summary and better formatting for homeview

--- BudgetBuddy/HomeView.swift
+++ BudgetBuddy/HomeView.swift
@@ -6,19 +6,24 @@ struct HomeView: View {
     @Binding var spendingLimit: Float
     
     var body: some View {
-        let totalAmount = items.reduce(0) { $0 + $1.amount }
+        let totalIncome = items.filter { $0.amount > 0 }.reduce(0) { $0 + $1.amount }
+        let totalSpending = items.filter { $0.amount < 0 }.reduce(0) { $0 + $1.amount }
         let todayAmount = items.filter { Calendar.current.isDateInToday($0.timeAdded) }.reduce(0) { $0 + $1.amount }
         let weeklySpending = calculateWeeklySpending(for: selectedCategory, in: items)
         let scheduledAmount = spendingLimit + weeklySpending
+        
+        // Choose icon based on the value of scheduledAmount
+        let scheduledIconName = scheduledAmount >= 0 ? "creditcard.fill" : "creditcard.trianglebadge.exclamationmark.fill"
+        let scheduledIconColor = scheduledAmount >= 0 ? Color.gray : Color.yellow
 
         VStack(spacing: 20) {
             HStack(spacing: 15) {
-                DashboardItemView(iconName: "calendar.circle.fill", iconColor: .blue, title: "Today", count: String(format: "%.2f", todayAmount))
-                DashboardItemView(iconName: "calendar.badge.clock", iconColor: .red, title: "Scheduled", count: String(format: "%.2f", scheduledAmount))
+                DashboardItemView(iconName: "chart.bar.fill", iconColor: .blue, title: "Today", count: formatNumber(todayAmount))
+                DashboardItemView(iconName: scheduledIconName, iconColor: scheduledIconColor, title: "Remaining Budget", count: formatNumber(scheduledAmount))
             }
             HStack(spacing: 15) {
-                DashboardItemView(iconName: "tray.circle.fill", iconColor: .black, title: "All", count: String(format: "%.2f", totalAmount))
-                DashboardItemView(iconName: "checkmark.circle.fill", iconColor: .gray, title: "Completed", count: "0")
+                DashboardItemView(iconName: "arrowshape.up.circle.fill", iconColor: .green, title: "All Time Income", count: formatNumber(totalIncome))
+                DashboardItemView(iconName: "arrowshape.down.circle.fill", iconColor: .red, title: "All Time Spending", count: formatNumber(abs(totalSpending)))
             }
         }
         .padding(.horizontal, 40)
@@ -26,16 +31,27 @@ struct HomeView: View {
     }
 
     private func calculateWeeklySpending(for category: SpendingItem.Label, in items: [SpendingItem]) -> Float {
-        let currentWeek = Calendar.current.component(.weekOfYear, from: Date())
-        let currentYear = Calendar.current.component(.year, from: Date())
+        guard let startOfWeek = Calendar.current.date(from: Calendar.current.dateComponents([.yearForWeekOfYear, .weekOfYear], from: Date())) else {
+            return 0
+        }
         
         return items.filter { item in
-            let itemWeek = Calendar.current.component(.weekOfYear, from: item.timeAdded)
-            let itemYear = Calendar.current.component(.year, from: item.timeAdded)
-            return item.label == category && itemWeek == currentWeek && itemYear == currentYear
+            return item.label == category && item.timeAdded >= startOfWeek
         }.reduce(0) { $0 + $1.amount }
     }
+    
+    // Number formatting function
+    private func formatNumber(_ value: Float) -> String {
+        if abs(value) >= 500_000 {
+            return String(format: "%.3fM", value / 1_000_000)
+        } else if abs(value) >= 500 {
+            return String(format: "%.2fK", value / 1_000)
+        } else {
+            return String(format: "%.1f$", value)
+        }
+    }
 }
+
 
 
 
@@ -56,7 +72,7 @@ struct DashboardItemView: View {
                     .font(.title)
                 Spacer()
                 Text(count)
-                    .font(.title)
+                    .font(.title2)
                     .bold()
                     .foregroundColor(Color(UIColor.label))
                 Spacer().frame(width: 8)
